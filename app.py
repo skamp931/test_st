ea9e31fd30deeb36e946636c8e0381d0d271df13
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-11 23:07:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,49 +17,41 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 yf.pdr_override()
 
+df_code = pd.read_csv("data_j.csv")
+code_list = []
+
 def main():
-    status_area = st.empty()
 
-    count_down_sec = 3
-    for i in range(count_down_sec):
-        status_area.write(f"{count_down_sec -i}sec left")
+    for code in df_code["コード"]:
+        if (code > 9000 and code < 10000):
+            #print(df_code.query('コード == @code')["銘柄名"])
+            code_name = df_code.query('コード == @code')["銘柄名"]
+            print(str(code)+".T:",code_name)
 
-        time.sleep(1)
+            df = pdr.get_data_yahoo(str(code)+".T",start,end)
     
-    status_area.write("Done!"+f"{end}")
-
-    print("3770.T")
-        #print(df_code.query('コード == @code')["銘柄名"])
-    #code_name = df_code.query('コード == @code')["銘柄名"]
-
-    df = pdr.get_data_yahoo("3770.T",start,end)
-
-    df["SMA7"] = df["Close"].rolling(window=7).mean()
-    df["SMA14"] = df["Close"].rolling(window=14).mean()
-    df["SMA21"] = df["Close"].rolling(window=21).mean()
-    sdiff = np.diff(df["Close"])
-    sdiff_sign = ((sdiff[:-1] * sdiff[1:]) < 0) & (sdiff[:-1] > 0)
-    #print(round(df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0],3),"\n")
-
-    if df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0] > 1.1:
-
-        if df["Close"].tail(1)[0] < 500:
-            plt.plot(df["SMA7"],label="SMA7")
-            plt.plot(df["SMA14"],label="SMA14")
-            plt.plot(df["SMA21"],label="SMA21")
-            plt.plot(df["Close"],label="Close")
-            plt.plot(df["Close"][1:-1][sdiff_sign],"o")
-
-            plt.legend()
-            #plt.savefig(f"{code}_{code_name.values[0]}.jpg")
-            st.pyplot(plt.show())
-
-            plt.plot(df["SMA7"],label="SMA7")
-            plt.plot(df["Close"],label="Close")
-            st.pyplot(plt.show())
-
+            df["SMA7"] = df["Close"].rolling(window=7).mean()
+            df["SMA14"] = df["Close"].rolling(window=14).mean()
+            df["SMA21"] = df["Close"].rolling(window=21).mean()
+            sdiff = np.diff(df["Close"])
+            sdiff_sign = ((sdiff[:-1] * sdiff[1:]) < 0) & (sdiff[:-1] > 0)
+            #print(round(df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0],3),"\n")
     
-    st.balloons()
+            if df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0] > 1.1:
+    
+                if df["Close"].tail(1)[0] < 500:
+                    plt.plot(df["SMA7"],label="SMA7")
+                    plt.plot(df["SMA14"],label="SMA14")
+                    plt.plot(df["SMA21"],label="SMA21")
+                    plt.plot(df["Close"],label="Close")
+                    plt.plot(df["Close"][1:-1][sdiff_sign],"o")
+    
+                    plt.legend()
+                    plt.savefig(f"{code}_{code_name.values[0]}.jpg")
+                    st.pyplot(plt.show())
+                    code_list.append([code,str(code)+".T:",code_name])
+
+    st.write(code_list)
 
 if __name__ == "__main__":
     main()

90111c2309b48beb227aac2ddef8e19782af79b2
skamp931 <43207451+skamp931@users.noreply.github.com>
2023-12-11 23:31:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,15 +19,26 @@ yf.pdr_override()
 
 df_code = pd.read_csv("data_j.csv")
 code_list = []
+overwrite = st.empty()
+
+st.title("銘柄抽出ツール")
+st.text("以下の条件の銘柄を抽出する。番号とチャート図を表示する。最後にコード一覧を表示する。",
+        "１．移動平均7日曲線が1割以上上昇傾向 \n",
+       "２．500円以下")
 
 def main():
 
     if st.button("解析スタート") == True:
+        st.spinner("解析中.....)
+                 
         for code in df_code["コード"]:
+            with overWrite.container():
+                st.write("コード",code)
             if (code > 100 and code < 10000):
                 #print(df_code.query('コード == @code')["銘柄名"])
                 code_name = df_code.query('コード == @code')["銘柄名"]
                 print(str(code)+".T:",code_name)
+                st.write(str(code)+".T:",code_name)
     
                 df = pdr.get_data_yahoo(str(code)+".T",start,end)
         
@@ -37,20 +48,22 @@ def main():
                 sdiff = np.diff(df["Close"])
                 sdiff_sign = ((sdiff[:-1] * sdiff[1:]) < 0) & (sdiff[:-1] > 0)
                 #print(round(df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0],3),"\n")
-        
-                if df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0] > 1.1:
-        
-                    if df["Close"].tail(1)[0] < 500:
-                        plt.plot(df["SMA7"],label="SMA7")
-                        plt.plot(df["SMA14"],label="SMA14")
-                        plt.plot(df["SMA21"],label="SMA21")
-                        plt.plot(df["Close"],label="Close")
-                        plt.plot(df["Close"][1:-1][sdiff_sign],"o")
-        
-                        plt.legend()
-                        plt.savefig(f"{code}_{code_name.values[0]}.jpg")
-                        st.pyplot(plt.show())
-                        code_list.append([code,str(code)+".T:",code_name])
+
+                if len(df["SMA21"].tail(21)) >= 21:
+
+                    if df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0] > 1.1:
+            
+                        if df["Close"].tail(1)[0] < 500:
+                            plt.plot(df["SMA7"],label="SMA7")
+                            plt.plot(df["SMA14"],label="SMA14")
+                            plt.plot(df["SMA21"],label="SMA21")
+                            plt.plot(df["Close"],label="Close")
+                            plt.plot(df["Close"][1:-1][sdiff_sign],"o")
+            
+                            plt.legend()
+                            plt.savefig(f"{code}_{code_name.values[0]}.jpg")
+                            st.pyplot(plt.show())
+                            code_list.append([code,str(code)+".T:",code_name])
     
         st.write(code_list)
 

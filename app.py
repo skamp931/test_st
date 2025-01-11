6cf4ccfcb9d3655b6b296ac4ffa2639d4073c7f2
skamp931 <43207451+skamp931@users.noreply.github.com>
2025-01-11 20:59:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,8 @@ def main():
                     st.write("SMA21_2120>>")
                     st.write(df["SMA21"].tail)
                     st.write("SMA21_210")
-                    st.write(df["SMA21"].tail(1)[0])
+                    st.write(df["SMA21"].tail(21)[20])
+                    st.write(df["SMA21"].tail(21)[0])
                     if df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0] > 1.1:
                         st.write(df["Close"])            
                         if df["Close"].tail(1)[0] < 1.1:

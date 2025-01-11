4ce5cb2e45249bc1bf325420082bb5c595dea36b
skamp931 <43207451+skamp931@users.noreply.github.com>
2025-01-11 21:07:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def main():
                     st.write(df["SMA21"].tail(21)[20]/df["SMA21"].tail(21)[0])
                     if df["SMA21"].tail(21)[20] / df["SMA21"].tail(21)[0] > 0.9:
                         st.write(df["Close"].tail(1))            
-                        if df["Close"].tail(1)[0] < 1.1:
+                        if df["Close"].tail(1)[0] < 1000:
                             st.write(str(code)+".T:",code_name)
 
                             plt.plot(df["SMA7"],label="SMA7")

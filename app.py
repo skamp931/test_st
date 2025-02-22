68bcf54fac3865d7df92c5a8f3380a5fe50cd3a2
skamp931 <mythappy622@gmail.com>
2025-02-22 12:29:02 +0900

    update date

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def main():
                             ax.plot(df.index, df["Close"], label="Close")
                             
                             # 修正: sdiff_signのインデックスを修正
-                            st.write(df)
+                            st.write(df.index)
 #                            st.write(df.index[1:][sdiff_sign])
 #                            st.write(df["Close"].iloc[1:][sdiff_sign])
 
